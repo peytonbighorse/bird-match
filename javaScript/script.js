910fa8bcbeeb5d2fd70a5fa6184f268585e66d69
pmb9308613 <162384210+pmb9308613@users.noreply.github.com>
2025-06-15 14:24:50 -0700

    Moved birdData into new js file; moved js files into new folder, fixed score counting issue

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,170 +1,62 @@
 //Notes and To-Dos
-//1. Score adds 1, then 2, then 3, etc. after each round
+//🌞 1. Score adds 1, then 2, then 3, etc. after each round
 //2. only cycles through 9 of 10 birds in birdData array
 //3. Need to fix styling so sizing of elements changes with size of screen
 //  3a. Scissor-tailed fly catcher options button overflows outside of button
 //  3b. Want font to get smaller - not squished when changing screen size
+//4. Move birdData to anotehr file
+//5. Separate concerns:
+//  5a. updateScore() - update score and score elements
+//  5b. resetQuestion() - reset styles, modals
+//  5c. displayBird() - show data from birdData, populate options buttons
+//  5d. endGame() - hide game page, show game over screen, reset game variables
 
 //Game State Variables
 let highscore = 0;
 let score = 0;
+let currentCorrectAnswer;
 
-//Bird Data
-let birdData = [
-  {
-    name: "stellers-jay",
-    image: "assets/bird-pics/stellers-jay.jfif",
-    correctAnswer: "STELLER'S JAY",
-    options: [
-      "BLUE JAY",
-      "STELLER'S JAY",
-      "NORTHERN CARDINAL",
-      "EURASIAN MAGPIE",
-    ],
-    tips: [
-      "Dark blue and black plumge with a crest",
-      "Small spots on forehead and near eyes",
-      "Broad, rounded wings",
-      "A harsh 'shack, shack, shack' sound",
-    ],
-    alt: "Steller's jay perched on a tree branch",
-  },
-  {
-    name: "brown-headed-cowbird",
-    image: "assets/bird-pics/brown-headed-cowbird.jfif",
-    correctAnswer: "BROWN-HEADED COWBIRD",
-    options: [
-      "BROWN-HEADED COWBIRD",
-      "BREWER'S BLACKBIRD",
-      "COMMON GRACKLE",
-      "BRONZED COWBIRD",
-    ],
-    tips: ["fake tip"],
-    alt: "Brown-headed cowbird",
-  },
-  {
-    name: "coopers-hawk",
-    image: "assets/bird-pics/coopers-hawk.jfif",
-    correctAnswer: "COOPER'S HAWK",
-    options: [
-      "COOPER'S HAWK",
-      "SHARP-SHINNED HAWK",
-      "PEREGRINE FALCOLN",
-      "AMERICAN GOSHAWK",
-    ],
-    tips: ["fake tip"],
-    alt: "Cooper's Hawk",
-  },
-  {
-    name: "dark-eyed-junco",
-    image: "assets/bird-pics/dark-eyed-junco",
-    correctAnswer: "DARK-EYED JUNCO",
-    options: [
-      "BLACK-CAPPED CHICKADEE",
-      "DARK-EYED JUNCO",
-      "BLACK PHOEBE",
-      "SPOTTED TOWHEE",
-    ],
-    tips: ["fake tip"],
-    alt: "Dark-eyed junco",
-  },
-  {
-    name: "great-blue-heron",
-    image: "assets/bird-pics/great-blue-heron.jfif",
-    correctAnswer: "GREAT BLUE HERON",
-    options: [
-      "GREAT BLUE HERON",
-      "LITTLE BLUE HERON",
-      "TRICOLORED HERON",
-      "NIGHT HERON",
-    ],
-    tips: ["fake tip"],
-    alt: "Great blue heron",
-  },
-  {
-    name: "northern-cardinal",
-    image: "assets/bird-pics/northern-cardinal.jfif",
-    correctAnswer: "NORTHERN CARDINAL",
-    options: [
-      "NORTHERN CARDINAL",
-      "SUMMER TANAGER",
-      "SCARLET TANAGER",
-      "VERMILION CARDINAL",
-    ],
-    tips: ["fake tip"],
-    alt: "Northern cardinal",
-  },
-  {
-    name: "northern-flicker",
-    image: "assets/bird-pics/northern-flicker.jfif",
-    correctAnswer: "NORTHERN FLICKER",
-    options: [
-      "PILEATED WOODPECKER",
-      "GILDED FLICKER",
-      "NORTHERN FLICKER",
-      "GILA WOODPECKER",
-    ],
-    tips: ["fake tip"],
-    alt: "Norther flicker",
-  },
-  {
-    name: "osprey",
-    image: "assets/bird-pics/osprey.jfif",
-    correctAnswer: "OSPREY",
-    options: ["OSPREY", "RED-TAILED HAWK", "BALD EAGLE", "GOLDEN EAGLE"],
-    tips: ["fake tip"],
-    alt: "Osprey soaring over body of water",
-  },
-  {
-    name: "sandwich-tern",
-    image: "assets/bird-pics/sandwich-tern.jfif",
-    correctAnswer: "SANDWICH TERN",
-    options: ["COMMON TERN", "ROYAL TERN", "ELEGANT TERN", "SANDWICH TERN"],
-    tips: ["fake tip"],
-    alt: "Sandwich tern",
-  },
-  {
-    name: "scissor-tailed-flycatcher",
-    image: "assets/bird-pics/scissor-tailed-flycatcher.jfif",
-    correctAnswer: "SCISSOR-TAILED FLYCATCHER",
-    options: [
-      "SCISSOR-TAILED FLYCATCHER",
-      "WESTERN KINGBIRD",
-      "FORK-TAILED FLYCATCHER",
-      "EASTERN KINGBIRD",
-    ],
-    tips: ["fake tip"],
-    alt: "Scissor tailed flycatcher perched on a fence",
-  },
-];
+//Pages
+const startPageSection = document.querySelector(".start-page");
+const gamePageSection = document.querySelector(".game-page");
+const gameOverSection = document.querySelector(".game-over");
+const aboutPageSection = document.querySelector(".about-and-credits");
 
 //DOM Elements
 const optionButtons = document.querySelectorAll(".option-btn");
 
+//Buttons
+const startGameBtn = document.querySelector(".start-game-btn");
+const aboutGameBtn = document.querySelector(".about-game-btn");
+const exitAboutBtn = document.querySelector(".exit-about-btn");
+const playAgainBtn = document.querySelector(".play-again-btn");
+const homescreenBtn = document.querySelector(".home-btn");
+const nextBirdBtn = document.querySelector(".next-bird-btn");
+
 //Core Functions
+optionButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    showAnswer(event.target, currentCorrectAnswer);
+  });
+});
 
 function loadBird() {
-  const gamePageSection = document.querySelector(".game-page");
-  const gameOverSection = document.querySelector(".game-over");
-  //Keeps track of which bird you’re on
   let randomNum = Math.floor(Math.random() * birdData.length);
-  let correctAnswer = birdData[randomNum].correctAnswer;
+  currentCorrectAnswer = birdData[randomNum].correctAnswer;
+
   //Load bird image
   let birdImageElement = document.querySelector(".bird-img");
   birdImageElement.innerHTML = `<img src="${birdData[randomNum].image}" alt="${birdData[randomNum].alt}" id="${birdData[randomNum].name}" style="width:320px; height:240px"/>`;
 
   //Populates the answer buttons
-  let optionButtonsElements = document.querySelectorAll(".option-btn");
+
   let optionsCounter = 0;
-  optionButtonsElements.forEach((button) => {
+  optionButtons.forEach((button) => {
+    currentCorrectAnswer = birdData[randomNum].correctAnswer;
     button.style.color = "#4c4027";
     button.style.backgroundColor = "#e9dabd";
     button.innerHTML = birdData[randomNum].options[optionsCounter];
     optionsCounter += 1;
-
-    button.addEventListener("click", (event) => {
-      showAnswer(button, correctAnswer);
-    });
   });
   //Update the birding tip
   let birdingTip = document.querySelector(".birding-tip");
@@ -237,22 +129,6 @@ function showAnswer(userChoice, correctChoice) {
   finalHighscoreElement.innerHTML = highscore;
   mainHighscore.innerHTML = highscore;
 }
-
-//Page Nav and Events
-
-//Buttons
-const startGameBtn = document.querySelector(".start-game-btn");
-const aboutGameBtn = document.querySelector(".about-game-btn");
-const exitAboutBtn = document.querySelector(".exit-about-btn");
-const playAgainBtn = document.querySelector(".play-again-btn");
-const homescreenBtn = document.querySelector(".home-btn");
-const nextBirdBtn = document.querySelector(".next-bird-btn");
-
-//Pages
-const startPageSection = document.querySelector(".start-page");
-const gamePageSection = document.querySelector(".game-page");
-const gameOverSection = document.querySelector(".game-over");
-const aboutPageSection = document.querySelector(".about-and-credits");
 
 //Nav Functions
 function startGame() {
