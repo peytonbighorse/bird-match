18ed9063fdddaddca94ed44f491fcda1d66bd1b3
pmb9308613 <162384210+pmb9308613@users.noreply.github.com>
2025-06-17 19:03:18 -0700

    Added resetBirdData array and credits to bird pictures

--- javaScript/script.js
+++ javaScript/script.js
@@ -36,7 +36,7 @@ function shuffleArray(array) {
 function displayBird(bird) {
   //Load bird image
   let birdImageElement = document.querySelector(".bird-img");
-  birdImageElement.innerHTML = `<img src="${bird.image}" alt="${bird.alt}" id="${bird.name}" style="width:clamp(200px, 40vw, 320px); height:auto"/>`;
+  birdImageElement.innerHTML = `<img src="${bird.image}" alt="${bird.alt}" id="${bird.name}" style="width:clamp(200px, 40vw, 320px); height:clamp(150px, 40vw, 240px)"/><figcaption class="bird-img-caption">${bird.imageCredit}</figcaption>`;
 
   //Populates the answer buttons
   let shuffledOptions = shuffleArray([...bird.options]);
@@ -170,148 +170,7 @@ function resetGame() {
   scoreElement.innerHTML = 0;
   nextBirdBtn.classList.remove("hidden");
   gameOverBtn.classList.add("hidden");
-  birdData = [
-    {
-      name: "stellers-jay",
-      image: "assets/bird-pics/stellers-jay.jfif",
-      correctAnswer: "STELLER'S JAY",
-      options: [
-        "BLUE JAY",
-        "STELLER'S JAY",
-        "NORTHERN CARDINAL",
-        "EURASIAN MAGPIE",
-      ],
-      tips: [
-        "Dark blue and black plumge with a crest",
-        "Small spots on forehead and near eyes",
-        "Broad, rounded wings",
-        "A harsh 'shack, shack, shack' sound",
-      ],
-      alt: "Steller's jay perched on a tree branch",
-    },
-    {
-      name: "brown-headed-cowbird",
-      image: "assets/bird-pics/brown-headed-cowbird.jfif",
-      correctAnswer: "BROWN-HEADED COWBIRD",
-      options: [
-        "BROWN-HEADED COWBIRD",
-        "BREWER'S BLACKBIRD",
-        "COMMON GRACKLE",
-        "BRONZED COWBIRD",
-      ],
-      tips: ["Males have a glossy brown head contrasting with a black body"],
-      alt: "Brown-headed cowbird",
-    },
-    {
-      name: "coopers-hawk",
-      image: "assets/bird-pics/coopers-hawk.jfif",
-      correctAnswer: "COOPER'S HAWK",
-      options: [
-        "COOPER'S HAWK",
-        "SHARP-SHINNED HAWK",
-        "PEREGRINE FALCOLN",
-        "AMERICAN GOSHAWK",
-      ],
-      tips: [
-        "Look for the rounded tail and orange eyes—larger than a Sharp-shinned Hawk",
-      ],
-
-      alt: "Cooper's Hawk",
-    },
-    {
-      name: "dark-eyed-junco",
-      image: "assets/bird-pics/dark-eyed-junco",
-      correctAnswer: "DARK-EYED JUNCO",
-      options: [
-        "BLACK-CAPPED CHICKADEE",
-        "DARK-EYED JUNCO",
-        "BLACK PHOEBE",
-        "SPOTTED TOWHEE",
-      ],
-      tips: ["Flashes of white outer tail feathers when it flies"],
-      alt: "Dark-eyed junco",
-    },
-    {
-      name: "great-blue-heron",
-      image: "assets/bird-pics/great-blue-heron.jfif",
-      correctAnswer: "GREAT BLUE HERON",
-      options: [
-        "GREAT BLUE HERON",
-        "LITTLE BLUE HERON",
-        "TRICOLORED HERON",
-        "NIGHT HERON",
-      ],
-      tips: [
-        "Huge wading bird with a long neck and dagger-like bill; often seen still as a statue",
-      ],
-      alt: "Great blue heron",
-    },
-    {
-      name: "northern-cardinal",
-      image: "assets/bird-pics/northern-cardinal.jfif",
-      correctAnswer: "NORTHERN CARDINAL",
-      options: [
-        "NORTHERN CARDINAL",
-        "SUMMER TANAGER",
-        "SCARLET TANAGER",
-        "VERMILION CARDINAL",
-      ],
-      tips: [
-        "Often seen on the ground, with a spotted belly and a black crescent on the chest",
-      ],
-      alt: "Northern cardinal",
-    },
-    {
-      name: "northern-flicker",
-      image: "assets/bird-pics/northern-flicker.jfif",
-      correctAnswer: "NORTHERN FLICKER",
-      options: [
-        "PILEATED WOODPECKER",
-        "GILDED FLICKER",
-        "NORTHERN FLICKER",
-        "GILA WOODPECKER",
-      ],
-      tips: [
-        "Black bars on the back and a white rump that flashes as it flies",
-      ],
-      alt: "Norther flicker",
-    },
-    {
-      name: "osprey",
-      image: "assets/bird-pics/osprey.jfif",
-      correctAnswer: "OSPREY",
-      options: ["OSPREY", "RED-TAILED HAWK", "BALD EAGLE", "GOLDEN EAGLE"],
-      tips: [
-        "Dark brown back with white underparts and a distinctive black eye stripe",
-      ],
-      alt: "Osprey soaring over body of water",
-    },
-    {
-      name: "sandwich-tern",
-      image: "assets/bird-pics/sandwich-tern.jfif",
-      correctAnswer: "SANDWICH TERN",
-      options: ["COMMON TERN", "ROYAL TERN", "ELEGANT TERN", "SANDWICH TERN"],
-      tips: [
-        "Black cap and a long black bill with a yellow tip; often seen in coastal areas",
-      ],
-      alt: "Sandwich tern",
-    },
-    {
-      name: "scissor-tailed-flycatcher",
-      image: "assets/bird-pics/scissor-tailed-flycatcher.jfif",
-      correctAnswer: "SCISSOR-TAILED FLYCATCHER",
-      options: [
-        "SCISSOR-TAILED FLYCATCHER",
-        "WESTERN KINGBIRD",
-        "FORK-TAILED FLYCATCHER",
-        "EASTERN KINGBIRD",
-      ],
-      tips: [
-        "Exceptionally long tail feathers that open and close like scissors during flight",
-      ],
-      alt: "Scissor tailed flycatcher perched on a fence",
-    },
-  ];
+  birdData = resetBirdData;
 }
 
 function endGame() {
